d7e74b6ac965b2fed276f97c703ca1a84943f5a6
Mark Grant <markrgrant@yahoo.com>
2015-10-07 23:28:19 -0700

    remove unused files

--- LexicalSpec.hs
+++
@@ -1,5 +0,0 @@
-
-
-type LexicalSpec = [(TokenClass, RegExp)]
-data TokenClass = Identifier | Number | Operator | Keyword | Whitespace |
-                  OpenParen | CloseParen | Semicolon | Assignment
